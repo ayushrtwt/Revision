7b57f044952158ae1a14ec1e1316bafff7324e7c
Ayush Rawat <ayushrawat3189@gmail.com>
2023-03-01 17:10:37 +0530

    java file added

---
+++ LearnJava/src/com/ayush/Idk.java
@@ -0,0 +1,11 @@
+package com.ayush;
+
+public class Idk {
+    public static void main(String[] args) {
+        System.out.println("before hi");
+        sayHi();
+    }
+    public static void sayHi(){
+        System.out.println("hello");
+    }
+}
