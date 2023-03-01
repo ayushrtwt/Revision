5a913576ff6329368311fd7563b58b4d70a5b93b
Ayush Rawat <ayushrawat3189@gmail.com>
2023-03-01 17:18:42 +0530

    creating classes and methods

---
+++ revision/src/com/ayush/Main.java
@@ -0,0 +1,8 @@
+package com.ayush;
+
+public class Main {
+    public static void main(String[] args) {
+        Me.sayName();
+        Me.sayAge();
+    }
+}
---
+++ revision/src/com/ayush/Me.java
@@ -0,0 +1,10 @@
+package com.ayush;
+
+public class Me {
+    public static void sayName(){
+        System.out.println("My name is Ayush");
+    }
+    public static void sayAge(){
+        System.out.println("My age is 19");
+    }
+}
