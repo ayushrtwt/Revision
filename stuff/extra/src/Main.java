ad7ada997c6567d387ad22d9cc472c0e56dda679
Ayush Rawat <ayushrawat3189@gmail.com>
2023-03-02 23:14:35 +0530

    added extra

---
+++ stuff/extra/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isAlive= true;
+        if(isAlive){
+            System.out.println("It's alive");
+        }
+        else {
+            System.out.println("Not alive");
+        }
+    }
+}
